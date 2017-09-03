8b1a693902c793b4ab461152754e212cf7027a61
hououdk <hjorthdk@gmail.com>
2017-09-03 23:08:39 +0200

    Add files via upload

--- php/check-login.php
+++ php/check-login.php
@@ -11,25 +11,25 @@ if(isset($_POST['login'])){
 	or die('Error: missing string');
 	
 	
-	$sql="SELECT trainer_id FROM `trainer` WHERE trainer_username=? and trainer_password=?";
+	$sql="SELECT trainer_id, trainer_password FROM `trainer` WHERE trainer_username=?";
 	$stmt = $link->prepare($sql);
-	$stmt->bind_param('ss', $username, $password);
-	$stmt->bind_result($trainerid);
+	$stmt->bind_param('s', $username);
+	$stmt->bind_result($trainerid, $trainerpass);
 	$stmt->execute();
+	// printf("Error: %s.\n", $stmt->error);
 	$stmt->store_result();
 	$rows = $stmt->num_rows;
 	while($stmt->fetch()){
-		
 		if($rows == 1){
-			$_SESSION['userid'] = $trainerid;
-			$_SESSION['user'] = $username;	
-			$_SESSION['login'] = 1;	
-			$message = "Logged in";
-		}
-		
-		else{
-			$_SESSION['login'] = 0;	
-			$message = "Not logged in";
+			if(password_verify($password, $trainerpass)){
+				$_SESSION['userid'] = $trainerid;
+				$_SESSION['user'] = $username;	
+				$_SESSION['login'] = 1;	
+				$message = "Logged in";
+			} else {
+				$_SESSION['login'] = 0;	
+				$message = "Not logged in";
+			}
 		}
 	}
 }
--- php/register-user.php
+++ php/register-user.php
@@ -4,7 +4,7 @@ if(isset($_POST["register"])){
 	$trainername = filter_input(INPUT_POST, 'trainername', FILTER_SANITIZE_STRING) or die('Error: missing type name');
 	$username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING) or die('Error: missing type name');
 	$password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING) or die('Error: missing type name');
-
+	$password = password_hash($password, PASSWORD_DEFAULT);
 
 	$sql="SELECT trainer_name FROM `trainer` WHERE trainer_username=?";
 	$stmt = $link->prepare($sql);
@@ -23,7 +23,6 @@ if(isset($_POST["register"])){
 		$stmt2 = $link->prepare($sql2);
 		$stmt2->bind_param('sss', $trainername, $username, $password);
 		$stmt2->execute();
-		printf("fejl : %s", $stmt2->error);
 		$message = $username. ' er blevet oprettet.';
 	}
 		
